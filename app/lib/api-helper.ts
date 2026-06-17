52353b893b7fbc26d63e63ad612eaa218ad3f3c9
Muhamed Essam <muhamed.essam2@aiesec.net>
2026-06-17 09:05:28 +0300

    getbaseURL fix

--- app/lib/api-helper.ts
+++ app/lib/api-helper.ts
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_APP_URL) {
-    return process.env.NEXT_PUBLIC_APP_URL;
+    return `https://${process.env.NEXT_PUBLIC_APP_URL}`;
   }
 
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
